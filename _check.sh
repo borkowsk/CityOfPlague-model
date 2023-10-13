7a6af0f2206f2b7797ce5a3b7e1d72e308d236f0
Wojciech T. Borkowski <wborkowsk@gmail.com>
2023-10-13 14:44:29 +0200

    ANSI_colors_used

--- _check.sh
+++ _check.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
-#Checking for required dependencies
-#EDIT=nano
-
-# https://intoli.com/blog/exit-on-errors-in-bash-scripts/
-set -e 
+## Checking for required dependencies
+## @author wborkowski@uw.edu.pl
 echo "Running" `realpath $0`
-echo -e "\n\tThis script stops on any error!\n\tWhen it stop, remove source of the error & run it again!\n"
-     
-echo -e "Test for required software:\n" 
-echo -e "Processing.org..."
+
+#echo "COLOR1="$COLOR1
+source $(dirname "$0")/screen.ini > screen.lst
+#echo "COLOR1="$COLOR1
+
+set -e #https://intoli.com/blog/exit-on-errors-in-bash-scripts/
+
+echo -e $COLOR2"\n\tThis script stops on any error!"
+echo -e "\tWhen it stop, remove source of the error & run it again!\n"$NORMCO
+
+echo -e $COLOR1"Test for required software:\n"$COLOR4
+echo -e "Processing.org..."$NORMCO
 
 find ~/ -name "processing" -type f -executable -print > processing_dirs.lst
 grep --color "processing" processing_dirs.lst
 wc -l processing_dirs.lst
 
-echo -e "\n\tLooks like you have Processing."
-echo -e "\tRemember to run install in its main directory."
+echo -e $COLOR2"\n\tLooks like you have Processing."
+echo -e "\tRemember to run install in its main directory."$NORMCO
 
-echo -e "\nVideo library..."exp
+echo -e $COLOR4"\nVideo library..." $NORMCO
 find ~/ -name "hamoid"  -print > hamoid_dirs.lst
 grep --color "hamoid" hamoid_dirs.lst
 wc -l hamoid_dirs.lst
 
-echo -e "\n\tLooks like you have Hamoid Video Library."
+echo -e $COLOR2"\n\tLooks like you have Hamoid Video Library."$NORMCO
 
-echo -e "\nffmpeg tool..."
+echo -e $COLOR4"\nffmpeg tool..."$NORMCO
 ffmpeg -version | grep --color "ffmpeg.*version"
-echo -e "\n\tLooks like you have ffmpeg tool instaled\n"
+echo -e $COLOR2"\n\tLooks like you have ffmpeg tool instaled\n"$NORMCO
 
 
 #instalacja zmiennej ze ścieżką do IDE processingu
@@ -43,3 +48,5 @@ then
 fi
 echo "In $HOME/.profile:"
 grep --color "PRIDE" $HOME/.profile
+
+
